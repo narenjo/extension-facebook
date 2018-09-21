3bb70625bf03de76796a62571341875f96ee7352
smival <smival@gmail.com>
2018-09-22 01:17:29 +0300

    fix: add imports

--- dependencies/android/facebook-sdk/src/org/haxe/extension/facebook/FacebookExtension.java
+++ dependencies/android/facebook-sdk/src/org/haxe/extension/facebook/FacebookExtension.java
@@ -49,6 +49,11 @@ import org.haxe.lime.HaxeObject;
 import java.math.BigDecimal;
 import java.util.Currency;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import java.lang.reflect.Type;
+import java.util.Map;
+
 public class FacebookExtension extends Extension {
 
 	static AccessTokenTracker accessTokenTracker;
